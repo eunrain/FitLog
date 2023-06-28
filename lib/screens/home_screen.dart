ecd2074d562dd3e7bc9417d4fd5f035e7147a1c3
goeunrain <boniteterain@gmail.com>
2023-06-29 00:40:18 +0900

    feat : 홈스크린 UI

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,7 @@
-import 'package:fitlog/widgets/calendar_widget.dart';
 import 'package:flutter/material.dart';
-import 'package:table_calendar/table_calendar.dart';
-import 'package:provider/provider.dart';
+import 'package:fitlog/widgets/calendar_widget.dart';
 import 'package:fitlog/components/appbar.dart';
 import 'package:fitlog/components/sidemenu.dart';
-import 'package:fitlog/provider/provider.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -16,14 +13,106 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final Map<DateTime, List<String>> _mealPlan = {};
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: MyAppBar(),
-      drawer: SideMenu(),
-      body: Column(
-        children: [CalendarWidget()],
+    return Scaffold(
+      appBar: const MyAppBar(),
+      drawer: const SideMenu(),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const CalendarWidget(),
+            const SizedBox(height: 16),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  const Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      '오늘의 식사',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  _mealBox(DateTime.now(), '아침'),
+                  const SizedBox(height: 20),
+                  _mealBox(DateTime.now(), '점심'),
+                  const SizedBox(height: 20),
+                  _mealBox(DateTime.now(), '저녁'),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
+    );
+  }
+
+  Widget _mealBox(DateTime date, String mealType) {
+    final meals = _mealPlan[date] ?? [];
+
+    return Container(
+      width: 1000,
+      height: 150,
+      decoration: BoxDecoration(
+        color: Colors.grey[200],
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: InkWell(
+        onTap: () {
+          _mealInput(date, mealType);
+        },
+        child: meals.isNotEmpty
+            ? Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Icon(Icons.check, color: Colors.green),
+                  const SizedBox(height: 4),
+                  Text(
+                    mealType,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),
+                ],
+              )
+            : const Icon(Icons.add, color: Colors.blue),
+      ),
+    );
+  }
+
+  void _mealInput(DateTime date, String mealType) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        String meal = '';
+
+        return AlertDialog(
+          title: const Text('식단 추가'),
+          content: TextField(
+            onChanged: (value) {
+              meal = value;
+            },
+            decoration: const InputDecoration(
+              hintText: '식단 입력',
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {},
+              child: const Text('추가'),
+            ),
+          ],
+        );
+      },
     );
   }
 }
--- lib/widgets/calendar_widget.dart
+++ lib/widgets/calendar_widget.dart
@@ -4,20 +4,37 @@ import 'package:table_calendar/table_calendar.dart';
 class CalendarWidget extends StatefulWidget {
   const CalendarWidget({super.key});
 
+  @override
   State<CalendarWidget> createState() {
-    return _CalendarWidget();
+    return _CalendarWidgetState();
   }
 }
 
-class _CalendarWidget extends State<CalendarWidget> {
-  final _now = DateTime.now();
+class _CalendarWidgetState extends State<CalendarWidget> {
+  DateTime? selectedDate;
 
   @override
   Widget build(BuildContext context) {
-    return TableCalendar(
-      focusedDay: DateTime.now(),
-      firstDay: DateTime(_now.year - 1, _now.month, _now.day),
-      lastDay: DateTime(_now.year + 1, _now.month, _now.day),
+    final DateTime now = DateTime.now();
+    final DateTime firstDay = DateTime(now.year - 1, now.month, now.day);
+    final DateTime lastDay = DateTime(now.year + 1, now.month, now.day);
+
+    return Column(
+      children: [
+        TableCalendar(
+          focusedDay: DateTime.now(),
+          firstDay: firstDay,
+          lastDay: lastDay,
+          selectedDayPredicate: (day) {
+            return isSameDay(selectedDate, day);
+          },
+          onDaySelected: (selectedDay, focusedDay) {
+            setState(() {
+              selectedDate = selectedDay;
+            });
+          },
+        ),
+      ],
     );
   }
 }
